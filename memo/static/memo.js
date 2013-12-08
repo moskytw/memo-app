53202f446d98b25c00272aa001fd1ee66f509ba3
Mosky <mosky.tw@gmail.com>
2013-12-08 23:26:31 +0800

    added memo's JavaScript client

---
+++ memo/static/memo.js
@@ -0,0 +1,52 @@
+(function() {
+
+var Memo = window.Memo = function (obj) {
+    this.obj = {
+        memo_id: null,
+        content: null
+    };
+    $.extend(this.obj, obj);
+};
+
+Memo.template = _.template(
+    '<article class="memo">'+
+        '<a class="button delete">x</a>'+
+        '<div class="content" contenteditable>'+
+            '<%- content %>'+
+        '</div>'+
+    '</article>'
+);
+
+Memo.prototype.remote = function (action) {
+
+    var _this = this;
+    return $.ajax('/api/memo/'+action, {
+        type: 'POST',
+        data: JSON.stringify(_this.obj),
+        contentType: 'application/json'
+    }).done(function(remote_obj, textStatus, jqXHR) {
+        _this.obj = remote_obj;
+    });
+
+};
+
+Memo.prototype.controller = function() {
+
+    var $memo = $(Memo.template(this.obj));
+
+    var $content = $memo.children('.content');
+
+    var _this = this;
+    $memo.on('input', function() {
+        _this.obj.content = $content.html();
+        _this.remote('sync');
+    }).on('click', '.delete', function(evt) {
+        _this.remote('delete');
+        $memo.remove();
+    });
+
+    return $memo;
+
+};
+
+})();
