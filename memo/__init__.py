866de9a4eb2569759a1abb08e883cdfbf7d894c0
Mosky <mosky.tw@gmail.com>
2013-12-08 23:24:59 +0800

    added memo's Python API

--- memo/__init__.py
+++ memo/__init__.py
@@ -7,3 +7,6 @@ app = Flask(__name__)
 
 from . import pages
 app.register_blueprint(pages.blueprint)
+
+from . import api_memo
+app.register_blueprint(api_memo.blueprint, url_prefix='/api/memo')
---
+++ memo/api_memo.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import json
+from flask import Blueprint, request, abort, jsonify
+from .models import memo as memo_model
+
+blueprint = Blueprint('api_memo', __name__)
+
+@blueprint.route('/sync', methods=['POST'])
+def sync():
+
+    memo = json.loads(request.data)
+    memo_id = memo.get('memo_id')
+
+    if memo_id is None:
+        memo_id = memo['memo_id'] = memo_model.next_memo_id
+        memo_model.next_memo_id += 1
+
+    memo_model.memos[memo_id] = memo
+
+    return jsonify(memo)
+
+@blueprint.route('/delete', methods=['POST'])
+def delete():
+
+    remote_memo = json.loads(request.data)
+
+    try:
+        del memo_model.memos[remote_memo['memo_id']]
+    except KeyError:
+        abort(400)
+
+    return jsonify()
